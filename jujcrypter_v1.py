58bdbbf88253fc2a221ff762087de3563c530c3f
SepyMovasat <81220038+SepyMovasat@users.noreply.github.com>
2021-07-04 19:18:18 +0430

    Create jujcrypter_v1.py

---
+++ jujcrypter_v1.py
@@ -0,0 +1,209 @@
+# Any issues ? Report on github
+import textwrap
+import os
+import time
+# clear screen
+def cls():
+    if os.name == 'posix':
+        _ = os.system('clear')
+    else:
+        _ = os.system('cls')
+# Error1
+def Error1():
+	print("KEY ERROR: At least one character can not be used(For example: \ )! Wait for updates in the future...")
+	time.sleep(7)
+	main_menu()
+# Error2
+def Error2():
+	print("Wrong input: Please enter 1 or 2 or 3 | Not a string !")
+	time.sleep(5)
+	main_menu()
+# Error3
+def Error3():
+	print("Wrong input: Please enter 1 or 2 or 3 | Not other numbers !")
+	time.sleep(5)
+	main_menu()
+# Encrypter
+def enc():
+	try:
+		alphabet = {
+		'a': 'juj',
+		'b': 'lul',
+		'c': 'bub',
+		'd': 'zuz',
+		'e': 'sus',
+		'f': 'vuv',
+		'g': 'nun',
+		'h': 'egh',
+		'i': 'lal',
+		'j': 'jaj',
+		'k': 'bob',
+		'l': 'deh',
+		'm': 'juf',
+		'n': 'you',
+		'o': 'pas',
+		'p': 'heh',
+		'q': 'pop',
+		'r': 'wew',
+		's': 'viv',
+		't': 'jij',
+		'u': 'bib',
+		'v': 'tiz',
+		'w': 'miz',
+		'x': 'viz',
+		'y': 'pis',
+		'z': 'lil',
+		' ': 'uiu',
+		'.': 'bob',
+		'!': 'ogf',
+		':': '092',
+		')': '121',
+		'|': '874',
+		',': '987',
+		'?': '461',
+		'=': 'e97',
+		'+': 'p65',
+		'-': 'dy7',
+		'0': 'olf',
+		'1': 'ra5',
+		'2': 'er4',
+		'3': 'lk1',
+		'4': '2lo',
+		'5': '90i',
+		'6': 'ez3',
+		'7': 'l1o',
+		'8': 'ox1',
+		'9': 'lo1',
+		'#': 'zrc',
+		'@': 'por',
+		'$': 'fgr',
+		'%': 'wty',
+		'^': 'kjh',
+		'&': 'zkl',
+		'*': 'dfr',
+		'(': 'kqo',
+		'_': 'suh',
+		'/': 'dft',
+		'"': 'adm',
+		"'": 'vub',
+		';': 'ssy',
+		'}': 'fer',
+		'{': 'SUS',
+		'[': 'fwe',
+		']': 'sdl'
+		}
+
+		FUN = input('Enter your FUN sentence : ')
+		jujlist = []
+		majid = ''
+		for lol in FUN:
+			majid += alphabet[lol]
+		print("Encrypted message:",majid)
+		time.sleep(8)
+		main_menu()
+	except KeyError:
+		Error1()
+# Decrypter
+def dec():
+	alphabet = {
+		'juj': 'a',
+		'lul': 'b',
+		'bub': 'c',
+		'zuz': 'd',
+		'sus': 'e',
+		'vuv': 'f',
+		'nun': 'g',
+		'egh': 'h',
+		'lal': 'i',
+		'jaj': 'j',
+		'bob': 'k',
+		'deh': 'l',
+		'juf': 'm',
+		'you': 'n',
+		'pas': 'o',
+		'heh': 'p',
+		'pop': 'q',
+		'wew': 'r',
+		'viv': 's',
+		'jij': 't',
+		'bib': 'u',
+		'tiz': 'v',
+		'miz': 'w',
+		'viz': 'x',
+		'pis': 'y',
+		'lil': 'z',
+		'uiu': ' ',
+		'bob': '.',
+		'ofg': '!',
+		'092': ':',
+		'121': ')',
+		'874': '|',
+		'987': ',',
+		'461': '?',
+		'e97': '=',
+		'p65': '+',
+		'dy7': '-',
+		'olf': '0',
+		'ra5': '1',
+		'er4': '2',
+		'lk1': '3',
+		'2lo': '4',
+		'90i': '5',
+		'ez3': '6',
+		'l1o': '7',
+		'ox1': '8',
+		'lo1': '9',
+		'zrc': '#',
+		'por': '@',
+		'fgr': '$',
+		'wty': '%',
+		'kjh': '^',
+		'zkl': '&',
+		'dfr': '*',
+		'kqo': '(',
+		'suh': '_',
+		'dft': '/',
+		'adm': '"',
+		'vub': "'",
+		'ssy': ';',
+		'fer': '}',
+		'SUS': '{',
+		'fwe': '[',
+		'sdl': ']'
+		}
+
+
+
+	entry = input('Enter your FUN sentence: ')
+	listentry = textwrap.wrap(entry, 3)
+
+	jujbet = ''
+
+
+	for items in listentry:
+		jujbet += alphabet[items]
+
+	print("Decrypted message:",jujbet)
+	time.sleep(8)
+	main_menu()
+# Main menu
+def main_menu():
+	try:
+		cls()
+		print("Welcome to Jujcrypter!")
+		print("1: Encrypt message")
+		print("2: Decrypt message")
+		print("3: Exit")
+		print("A program by @SepyMovasat and @arkarimi")
+		main_inp = int(input("Which one ? [1,2,3] "))
+		if main_inp == 1:
+			enc()
+		elif main_inp == 2:
+			dec()
+		elif main_inp == 3:
+			exit()
+		else:
+			Error3()
+	except (ValueError,TypeError):
+		Error2()
+main_menu()
